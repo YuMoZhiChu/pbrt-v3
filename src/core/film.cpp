519f0236bc29a23fdd46a9b4890cc9c90431071e
御魔の触れる <YuMoZhiChu@users.noreply.github.com>
2020-08-12 10:57:07 +0800

    flim 学习完成

--- src/core/film.cpp
+++ src/core/film.cpp
@@ -65,6 +65,7 @@ Film::Film(const Point2i &resolution, const Bounds2f &cropWindow,
 
     // Allocate film image storage
 	// 为每一个像素，分配动态内存
+	// 这是我们要显示的内容的 Pixel
     pixels = std::unique_ptr<Pixel[]>(new Pixel[croppedPixelBounds.Area()]);
     filmPixelMemory += croppedPixelBounds.Area() * sizeof(Pixel);
 
@@ -106,14 +107,25 @@ Bounds2f Film::GetPhysicalExtent() const {
     return Bounds2f(Point2f(-x / 2, -y / 2), Point2f(x / 2, y / 2));
 }
 
+// 为了适应多线程的优化，这里提供了一个 Tile 的定义
+// 参数是传入一个 采样范围（样本范围），返回一个 unique 的 FileTile 指针
+// 每一块 FilmTile 指向了一部分的 像素信息，这一部分的信息是 这个对应范围内，对像素的贡献程度（也就是累加结果），而线程对这个 Tile 是 unique 掌控的
+// 所以不需要担心和其他线程的竞争关系
+// 完成了 Tile 上的工作后，我们会传回 Tile，并且做一次合并
 std::unique_ptr<FilmTile> Film::GetFilmTile(const Bounds2i &sampleBounds) {
     // Bound image pixels that samples in _sampleBounds_ contribute to
     Vector2f halfPixel = Vector2f(0.5f, 0.5f);
     Bounds2f floatBounds = (Bounds2f)sampleBounds;
+	// 给定了样本的范围，现在我们要计算，样本影响到的像素的对应范围，很显然，就是以样本为中心，扩大滤波器的范围
+	// 考量到 离散坐标转连续坐标的影响，这里参考 Sampling_Theory.html 部分，将离散的点，转换为连续的点的做法，为什么大的那部分也是减呢，因为我们做的是一个范围，而不是当个的点
+	// 加减滤波器的半径
     Point2i p0 = (Point2i)Ceil(floatBounds.pMin - halfPixel - filter->radius);
     Point2i p1 = (Point2i)Floor(floatBounds.pMax - halfPixel + filter->radius) +
                  Point2i(1, 1);
+	// 要和图形边界求交，图像外的像素无需考虑
+	// 这个范围，就是，给定的 样本范围，能够影响的 像素范围
     Bounds2i tilePixelBounds = Intersect(Bounds2i(p0, p1), croppedPixelBounds);
+	// 返回一个 FilmTile 指针
     return std::unique_ptr<FilmTile>(new FilmTile(
         tilePixelBounds, filter->radius, filterTable, filterTableWidth,
         maxSampleLuminance));
@@ -128,31 +140,45 @@ void Film::Clear() {
     }
 }
 
+// 渲染线程调用，合并 FilmTile，把计算结果叠加
+// 这里传入一个 FilmTile 的 u_q，所以在传入的时候，FilmTile 的拥有者，就是当前函数了
+// 完成了本次调用后，内存会被自动释放，就不可以再使用 FilmTile 并往里面写数据了
 void Film::MergeFilmTile(std::unique_ptr<FilmTile> tile) {
     ProfilePhase p(Prof::MergeFilmTile);
     VLOG(1) << "Merging film tile " << tile->pixelBounds;
+	// 用一个互斥锁，保证每次只有一次做数据更新
     std::lock_guard<std::mutex> lock(mutex);
+	// 只需要遍历，这一块，影响的 像素范围即可
     for (Point2i pixel : tile->GetPixelBounds()) {
         // Merge _pixel_ into _Film::pixels_
         const FilmTilePixel &tilePixel = tile->GetPixel(pixel);
         Pixel &mergePixel = GetPixel(pixel);
         Float xyz[3];
         tilePixel.contribSum.ToXYZ(xyz);
+		// 转成 xyz
+		// 合并到，我们 Film 类的 Pixel 中，这里存的 Pixel 是 XYZ
         for (int i = 0; i < 3; ++i) mergePixel.xyz[i] += xyz[i];
+		// 滤波器权重和相加，这个也是分母
         mergePixel.filterWeightSum += tilePixel.filterWeightSum;
     }
 }
 
+// 这个是给整个 Film，一次性提供所有的光谱值
 void Film::SetImage(const Spectrum *img) const {
     int nPixels = croppedPixelBounds.Area();
     for (int i = 0; i < nPixels; ++i) {
         Pixel &p = pixels[i];
-        img[i].ToXYZ(p.xyz);
+        img[i].ToXYZ(p.xyz); // 转成 xyz 直接传入
         p.filterWeightSum = 1;
         p.splatXYZ[0] = p.splatXYZ[1] = p.splatXYZ[2] = 0;
     }
 }
 
+// 一些光线传入算法，比如 16.3 会出现的  bidirectional path tracing
+// 需要一个 "Splat" 贡献度，可能是对任意一个像素的
+// 在这里，Splat 不是做加权求和，而是做简单的相加
+// 这里大致的想法是，如果一个像素，它旁边的 Splat 越多，他会越亮
+// Pixel::splatXYZ 是原子类型的 AtomicFloat，允许多个写出直接通过 AddSplat 相加，不用写锁
 void Film::AddSplat(const Point2f &p, Spectrum v) {
     ProfilePhase pp(Prof::SplatFilm);
 
@@ -177,23 +203,31 @@ void Film::AddSplat(const Point2f &p, Spectrum v) {
     Float xyz[3];
     v.ToXYZ(xyz);
     Pixel &pixel = GetPixel(pi);
+	// 直接相加即可，不做加权求和
     for (int i = 0; i < 3; ++i) pixel.splatXYZ[i].Add(xyz[i]);
 }
 
+// 将图像写入文件中
+// 传入一个 splat 缩放因子，这里会在 AddSplact 中用到，详细看 16.4.5 的内容
 void Film::WriteImage(Float splatScale) {
     // Convert image to RGB and compute final pixel values
     LOG(INFO) <<
         "Converting image to RGB and computing final weighted pixel values";
+	// 首先是分配对应大小，存储 rgb 的图像内容
     std::unique_ptr<Float[]> rgb(new Float[3 * croppedPixelBounds.Area()]);
     int offset = 0;
+	// 存储流程，遍历所有的像素点
     for (Point2i p : croppedPixelBounds) {
         // Convert pixel XYZ color to RGB
+		// 转换到 RGB，存储到 RGB 空间
         Pixel &pixel = GetPixel(p);
         XYZToRGB(pixel.xyz, &rgb[3 * offset]);
 
         // Normalize pixel with weight sum
         Float filterWeightSum = pixel.filterWeightSum;
+		// 除法计算
         if (filterWeightSum != 0) {
+			// 因为可能存在负数，所以要规范到 > 0
             Float invWt = (Float)1 / filterWeightSum;
             rgb[3 * offset] = std::max((Float)0, rgb[3 * offset] * invWt);
             rgb[3 * offset + 1] =
@@ -206,12 +240,14 @@ void Film::WriteImage(Float splatScale) {
         Float splatRGB[3];
         Float splatXYZ[3] = {pixel.splatXYZ[0], pixel.splatXYZ[1],
                              pixel.splatXYZ[2]};
+		// 同时计算 Splat，并做缩放计算
         XYZToRGB(splatXYZ, splatRGB);
         rgb[3 * offset] += splatScale * splatRGB[0];
         rgb[3 * offset + 1] += splatScale * splatRGB[1];
         rgb[3 * offset + 2] += splatScale * splatRGB[2];
 
         // Scale pixel value by _scale_
+		// 最终写入的像素，用户指定的系数，一般来说，都会是1????
         rgb[3 * offset] *= scale;
         rgb[3 * offset + 1] *= scale;
         rgb[3 * offset + 2] *= scale;
@@ -221,6 +257,7 @@ void Film::WriteImage(Float splatScale) {
     // Write RGB image
     LOG(INFO) << "Writing image " << filename << " with bounds " <<
         croppedPixelBounds;
+	// 调用 pbrt 的接口，如果写入的是8位整数格式，在其转换为整数前，会进行 gamma 校正 ????
     pbrt::WriteImage(filename, &rgb[0], croppedPixelBounds, fullResolution);
 }
 
--- src/core/film.h
+++ src/core/film.h
@@ -49,8 +49,13 @@
 namespace pbrt {
 
 // FilmTilePixel Declarations
+// FilmTile 的每一个像素，需要维护两个数，
 struct FilmTilePixel {
+	// the weighted contributions from the pixel samples (according to the reconstruction filter weights)
+	// pixel 样本的权重和
     Spectrum contribSum = 0.f;
+	// the filter weights
+	// filter 滤波器的权重和
     Float filterWeightSum = 0.f;
 };
 
@@ -122,6 +127,8 @@ class Film {
 class FilmTile {
   public:
     // FilmTile Public Methods
+	// 构造一个2D边界框，提供了最终的像素边界，以及做一些预计算
+	// filterTable 一个 float 指针，里面是 filter 预计算的值
     FilmTile(const Bounds2i &pixelBounds, const Vector2f &filterRadius,
              const Float *filterTable, int filterTableSize,
              Float maxSampleLuminance)
@@ -131,24 +138,37 @@ class FilmTile {
           filterTable(filterTable),
           filterTableSize(filterTableSize),
           maxSampleLuminance(maxSampleLuminance) {
+		// 初始化 pixelBounds 大小的 Pixel，每个 Piexl 里面有一个 FilmTilePixel 数据
         pixels = std::vector<FilmTilePixel>(std::max(0, pixelBounds.Area()));
     }
+	// 当一条射线的一个样本对应辐射度被计算时，积分器能会调用 AddSample
+	// 它的参数是
+	// 获取一个样本 pFilm位置 其辐射值 L 以及 Camera::GenerateRayDifferential 计算出的权重
+	// 这个函数的功能是，将该样本，对 Pixel 的共享结果，也就是滤波方程的计算结果，存在起来
     void AddSample(const Point2f &pFilm, Spectrum L,
                    Float sampleWeight = 1.) {
         ProfilePhase _(Prof::AddFilmSample);
         if (L.y() > maxSampleLuminance)
             L *= maxSampleLuminance / L.y();
         // Compute sample's raster bounds
+		// 计算 该样本影响的像素范围
+		// 连续转离散 -0.5 处理
         Point2f pFilmDiscrete = pFilm - Vector2f(0.5f, 0.5f);
+		// 滤波器范围
         Point2i p0 = (Point2i)Ceil(pFilmDiscrete - filterRadius);
         Point2i p1 =
             (Point2i)Floor(pFilmDiscrete + filterRadius) + Point2i(1, 1);
+		// 是否出界，出界就不用管了
         p0 = Max(p0, pixelBounds.pMin);
         p1 = Min(p1, pixelBounds.pMax);
 
         // Loop over filter support and add sample to pixel arrays
 
         // Precompute $x$ and $y$ filter table offsets
+		// 滤波器权重表已经算好了，都在 filterTable 指针中
+		// 这里做的是，我们做一个位置的映射表，这里节省的是
+		// 我们做 2次 p1.x + p1.y 的循环的计算即可
+		// 而不是在下面做 p1.x * p1.y 的循环次数 的 位置映射 的 计算
         int *ifx = ALLOCA(int, p1.x - p0.x);
         for (int x = p0.x; x < p1.x; ++x) {
             Float fx = std::abs((x - pFilmDiscrete.x) * invFilterRadius.x *
@@ -161,6 +181,7 @@ class FilmTile {
                                 filterTableSize);
             ify[y - p0.y] = std::min((int)std::floor(fy), filterTableSize - 1);
         }
+		// 计算在该样本影响下，该样本，对每个像素位置的贡献度
         for (int y = p0.y; y < p1.y; ++y) {
             for (int x = p0.x; x < p1.x; ++x) {
                 // Evaluate filter value at $(x,y)$ pixel
@@ -168,12 +189,14 @@ class FilmTile {
                 Float filterWeight = filterTable[offset];
 
                 // Update pixel values with filtered sample contribution
+				// 计算流程，就是累加贡献度 的内容
                 FilmTilePixel &pixel = GetPixel(Point2i(x, y));
                 pixel.contribSum += L * sampleWeight * filterWeight;
                 pixel.filterWeightSum += filterWeight;
             }
         }
     }
+	// 获取某个像素坐标下，我们累计的 Piexl 值
     FilmTilePixel &GetPixel(const Point2i &p) {
         CHECK(InsideExclusive(p, pixelBounds));
         int width = pixelBounds.pMax.x - pixelBounds.pMin.x;
@@ -181,6 +204,7 @@ class FilmTile {
             (p.x - pixelBounds.pMin.x) + (p.y - pixelBounds.pMin.y) * width;
         return pixels[offset];
     }
+	// const 版本
     const FilmTilePixel &GetPixel(const Point2i &p) const {
         CHECK(InsideExclusive(p, pixelBounds));
         int width = pixelBounds.pMax.x - pixelBounds.pMin.x;
@@ -188,6 +212,7 @@ class FilmTile {
             (p.x - pixelBounds.pMin.x) + (p.y - pixelBounds.pMin.y) * width;
         return pixels[offset];
     }
+	// 返回的是，它的 影响的像素 范围
     Bounds2i GetPixelBounds() const { return pixelBounds; }
 
   private:
